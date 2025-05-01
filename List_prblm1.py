8c4cfb98df2dfbdf91e0a80abccf4be4e8795850
Mir Athar Hussain <mirbotics@gmail.com>
2025-05-01 22:25:49 +0530

    Keyowrd find prblm

--- List_prblm1.py
+++ List_prblm1.py
@@ -1,1 +1,25 @@
-1st question
+#1st question:
+
+#keywords: while, if, else, for, continue, const, char,  switch, break, case, int, long, float
+
+#examples input and output:
+
+#input: where    input: for     input: in
+#output: No      output: Yes    output: No
+
+#Given keywords, check whether the given input is keyword or not.  if keyword print yes else no
+
+
+#! usr/bin/python
+
+def check_keyword(keyword_list, param):
+	if param in keyword_list:
+		print("Yes")
+	else:
+		print("No")
+	
+if __name__ =="__main__":
+	keyword_lst = ["while", "if", "else", "for", "continue", "const", "char",  "switch", "break", "case", "int", "long", "float"]
+	user_input = input("Enter a string")
+	check_keyword(keyword_lst, user_input)
+	
